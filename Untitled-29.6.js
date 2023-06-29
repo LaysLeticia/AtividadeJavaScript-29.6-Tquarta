76417133b18d4017a9d47670676547c3460badbe
LaysLeticia <136758166+LaysLeticia@users.noreply.github.com>
2023-06-29 16:30:23 -0300

    Add files via upload
    Atividade dia 29.6 T-quarta.

---
+++ Untitled-29.6.js
@@ -0,0 +1,53 @@
+//ATIVIDADE FIXACAO 29.6
+//QUESTAO 1-a 
+var x = 5;
+var y = 9;
+var resultado = ( x === y);
+console.log(resultado);
+//QUESTAO 1-b
+var x = 6;
+var y = "6";
+var resultado = ( x !== y);
+console.log(resultado);
+//QUESTAO 1-c
+var x = "7";
+var y = 7;
+var resultado = (x == y);
+console.log(resultado);
+
+//QUESTAO 2-a & b
+var nome = "Fulano";
+console.log("Boa noite, " + nome + "!");
+
+//QUESTAO 3-a
+var x = 10;
+var y = 5;
+x += y;
+console.log(x);
+//3-b
+var x = 10;
+var y = 5;
+x *= y;
+console.log(x);
+//3-c
+var x = 10;
+var y = 5;
+x -= x;
+console.log(x);
+//3-d
+var x = 10;
+var y = 5;
+x /= y;
+console.log(x);
+//3-e
+var x = 10;
+var y = 5;
+x -= y;
+console.log(x);
+
+
+
+
+
+
+
